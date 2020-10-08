5ae990a30d20a67fb2fe445126324e7cdc24dde5
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 13:34:56 +0200

    init

--- Open-Lab-02.05/Calculator.cs
+++ Open-Lab-02.05/Calculator.cs
@@ -4,9 +4,7 @@ namespace Open_Lab_02._05
 {
     public class Calculator
     {
-        public bool DivisibleByFive(int number)
-        {
-            throw new NotImplementedException();
-        }
+        public bool DivisibleByFive(int number) => (number % 5 == 0 ? true : false);
+        
     }
 }
